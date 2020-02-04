0c1db2bd5e4d210ff96c71adcae55d87e7b37176
Travis <60670527+bin0al@users.noreply.github.com>
2020-02-04 17:41:57 -0500

    Initial Project v1
    Project built locally; Initial upload

---
+++ Super_Metroid_World_Map/GeoJson/reserveTanks.js
@@ -0,0 +1,54 @@
+var reserveTanks = {
+  "type": "FeatureCollection",
+  "features": [
+
+//-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|
+
+    {
+    "type": "Feature",
+    "properties": {
+      "item": "ReserveTank",
+    },
+    "geometry": {
+      "type": "Point",
+      "coordinates": [2232,-5752]
+    }},
+        
+//-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|
+
+    {
+    "type": "Feature",
+    "properties": {
+      "item": "ReserveTank",
+    },
+    "geometry": {
+      "type": "Point",
+      "coordinates": [11304.5,-10360]
+    }},
+        
+//-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|
+
+    {
+    "type": "Feature",
+    "properties": {
+      "item": "ReserveTank",
+    },
+    "geometry": {
+      "type": "Point",
+      "coordinates": [11256.5,-8264.5]
+    }},
+        
+//-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|
+
+    {
+    "type": "Feature",
+    "properties": {
+      "item": "ReserveTank",
+    },
+    "geometry": {
+      "type": "Point",
+      "coordinates": [12088.5,-440]
+    }},
+  ]
+  
+};
