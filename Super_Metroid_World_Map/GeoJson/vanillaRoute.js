9b29fc897ec9c0ae21286d8259dda6eee2bf274f
Travis <60670527+bin0al@users.noreply.github.com>
2020-02-06 10:07:49 -0500

    Routing
    Added support for route layers, and added a test path

---
+++ Super_Metroid_World_Map/GeoJson/vanillaRoute.js
@@ -0,0 +1,16 @@
+var vanillaRoute = {
+  "type": "FeatureCollection",
+  "features": [
+    {
+    "type": "Feature",
+    "properties": {
+    },
+    "geometry": {
+      "type": "LineString",
+      "coordinates": [
+        [6523,-1072],
+        [6298,-1172]
+      ]
+    }},
+  ]
+};
