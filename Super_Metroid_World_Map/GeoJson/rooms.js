8447e13252a4d90c66c6e802eca583c6a5efad85
Travis <60670527+bin0al@users.noreply.github.com>
2020-02-18 16:05:02 -0500

    Room Layer Creation - Crateria
    Room Layers created for Crateria

--- Super_Metroid_World_Map/GeoJson/rooms.js
+++ Super_Metroid_World_Map/GeoJson/rooms.js
@@ -2,25 +2,614 @@ var rooms = {
   "type": "FeatureCollection",
   "features": [
 
-//-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Landing Site",
+        "link": "https://wiki.supermetroid.run/Landing_Site",
+        "area": "Crateria",
+      },
+      "geometry": {
+        "type": "Polygon",
+        "coordinates": [[
+          [5376, 0],
+          [5376, -1280],
+          [7680, -1280],
+          [7680, 0]
+        ]],
+      }
+    },
 
     {
-    "type": "Feature",
-    "properties": {
-      "name": "Landing Site",
-      "link": "https://wiki.supermetroid.run/Landing_Site",
+      "type": "Feature",
+      "properties": {
+        "name": "Crateria Power Bomb Room",
+        "link": "https://wiki.supermetroid.run/Crateria_Power_Bomb_Room",
+        "area": "Crateria",
+      },
+      "geometry": {
+        "type": "Polygon",
+        "coordinates": [[
+          [7680, -256],
+          [7680, -512],
+          [8192, -512],
+          [8192, -256]
+        ]],
+      }
     },
-    "geometry": {
-      "type": "Polygon",
-      "coordinates": [[
-        [5376, 0],
-        [5376, -1280],
-        [7680, -1280],
-        [7680, 0]
-      ]],
-    }},
-        
-//-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|
+
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Gauntlet Entrance",
+        "link": "https://wiki.supermetroid.run/Gauntlet_Entrance",
+        "area": "Crateria",
+      },
+      "geometry": {
+        "type": "Polygon",
+        "coordinates": [[
+          [4096, -512],
+          [4096, -768],
+          [5376, -768],
+          [5376, -512]
+        ]],
+      }
+    },
+
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Gauntlet Energy Tank Room",
+        "link": "https://wiki.supermetroid.run/Gauntlet_Energy_Tank_Room",
+        "area": "Crateria",
+      },
+      "geometry": {
+        "type": "Polygon",
+        "coordinates": [[
+          [2560, -512],
+          [2560, -768],
+          [4096, -768],
+          [4096, -512]
+        ]],
+      }
+    },
+
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Green Pirates Shaft",
+        "link": "https://wiki.supermetroid.run/Green_Pirates_Shaft",
+        "area": "Crateria",
+      },
+      "geometry": {
+        "type": "Polygon",
+        "coordinates": [[
+          [2304, -512],
+          [2304, -2304],
+          [2560, -2304],
+          [2560, -512]
+        ]],
+      }
+    },
+
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Terminator",
+        "link": "https://wiki.supermetroid.run/Terminator_Room",
+        "area": "Crateria",
+      },
+      "geometry": {
+        "type": "Polygon",
+        "coordinates": [[
+          [2560, -1536],
+          [2560, -1792],
+          [2816, -1792],
+          [3840, -1280],
+          [4096, -1280],
+          [4096, -1024],
+          [3840, -1024],
+          [2816, -1536]
+        ]],
+      }
+    },
+
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Parlor and Alcatraz",
+        "link": "https://wiki.supermetroid.run/Parlor_and_Alcatraz",
+        "area": "Crateria",
+      },
+      "geometry": {
+        "type": "Polygon",
+        "coordinates": [[
+          [4096, -1024],
+          [4096, -1280],
+          [4352, -1280],
+          [4352, -2304],
+          [4608, -2304],
+          [4608, -1280],
+          [4864, -1280],
+          [4864, -1792],
+          [5120, -1792],
+          [5120, -1280],
+          [5376, -1280],
+          [5376, -1024]
+        ]],
+      }
+    },
+
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Flyway",
+        "link": "https://wiki.supermetroid.run/Flyway",
+        "area": "Crateria",
+      },
+      "geometry": {
+        "type": "Polygon",
+        "coordinates": [[
+          [5120, -1536],
+          [5888, -1536],
+          [5888, -1792],
+          [5120, -1792]
+        ]],
+      }
+    },
+
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Bomb Torizo Room",
+        "link": "https://wiki.supermetroid.run/Bomb_Torizo_Room",
+        "area": "Crateria",
+      },
+      "geometry": {
+        "type": "Polygon",
+        "coordinates": [[
+          [5888, -1536],
+          [5888, -1792],
+          [6144, -1792],
+          [6144, -1536]
+        ]],
+      }
+    },
+
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Crateria Save Room",
+        "link": "https://wiki.supermetroid.run/Crateria_Save_Room",
+        "area": "Crateria",
+      },
+      "geometry": {
+        "type": "Polygon",
+        "coordinates": [[
+          [4096, -1536],
+          [4096, -1792],
+          [4352, -1792],
+          [4352, -1536]
+        ]],
+      }
+    },
+
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Final Missile Bombway",
+        "link": "https://wiki.supermetroid.run/Final_Missile_Bombway",
+        "area": "Crateria",
+      },
+      "geometry": {
+        "type": "Polygon",
+        "coordinates": [[
+          [3840, -1792],
+          [3840, -2048],
+          [4352, -2048],
+          [4352, -1792]
+        ]],
+      }
+    },
+
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "The Final Missile",
+        "link": "https://wiki.supermetroid.run/The_Final_Missile",
+        "area": "Crateria",
+      },
+      "geometry": {
+        "type": "Polygon",
+        "coordinates": [[
+          [3584, -1792],
+          [3584, -2048],
+          [3840, -2048],
+          [3840, -1792]
+        ]],
+      }
+    },
+
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Pre-Map Flyway",
+        "link": "https://wiki.supermetroid.run/Pre-Map_Flyway",
+        "area": "Crateria",
+      },
+      "geometry": {
+        "type": "Polygon",
+        "coordinates": [[
+          [4608, -1792],
+          [4608, -2048],
+          [5376, -2048],
+          [5376, -1792]
+        ]],
+      }
+    },
+
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Crateria Map Room",
+        "link": "https://wiki.supermetroid.run/Crateria_Map_Room",
+        "area": "Crateria",
+      },
+      "geometry": {
+        "type": "Polygon",
+        "coordinates": [[
+          [5376, -1792],
+          [5376, -2048],
+          [5632, -2048],
+          [5632, -1792]
+        ]],
+      }
+    },
+
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Lower Mushrooms",
+        "link": "https://wiki.supermetroid.run/Lower_Mushrooms",
+        "area": "Crateria",
+      },
+      "geometry": {
+        "type": "Polygon",
+        "coordinates": [[
+          [1280,-2048],
+          [1280, -2304],
+          [2304,-2304],
+          [2304, -2048]
+        ]],
+      }
+    },
+
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Green Brinstar Elevator Room",
+        "link": "https://wiki.supermetroid.run/Green_Brinstar_Elevator_Room",
+        "area": "Crateria",
+      },
+      "geometry": {
+        "type": "Polygon",
+        "coordinates": [[
+          [1024,-2048],
+          [1024, -2304],
+          [1280,-2304],
+          [1280, -2048]
+        ]],
+      }
+    },
+
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Crateria Tube",
+        "link": "https://wiki.supermetroid.run/Crateria_Tube",
+        "area": "Crateria",
+      },
+      "geometry": {
+        "type": "Polygon",
+        "coordinates": [[
+          [7680,-1024],
+          [7680, -2304],
+          [7936,-2304],
+          [7936, -1024]
+        ]],
+      }
+    },
+
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Crateria Kihunter Room",
+        "link": "https://wiki.supermetroid.run/Crateria_Kihunter_Room",
+        "area": "Crateria",
+      },
+      "geometry": {
+        "type": "Polygon",
+        "coordinates": [[
+          [7936,-1024],
+          [7936,-1280],
+          [8192,-1280],
+          [8192,-1792],
+          [8448,-1792],
+          [8448,-1280],
+          [8704,-1280],
+          [8704,-1024]
+        ]],
+      }
+    },
+
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Red Brinstar Elevator Room",
+        "link": "https://wiki.supermetroid.run/Red_Brinstar_Elevator_Room",
+        "area": "Crateria",
+      },
+      "geometry": {
+        "type": "Polygon",
+        "coordinates": [[
+          [8192,-1792],
+          [8448,-1792],
+          [8448,-2048],
+          [8192,-2048]
+        ]],
+      }
+    },
+
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Statues Hallway",
+        "link": "https://wiki.supermetroid.run/Statues_Hallway",
+        "area": "Crateria",
+      },
+      "geometry": {
+        "type": "Polygon",
+        "coordinates": [[
+          [2560,-2048],
+          [2560,-2304],
+          [3840,-2304],
+          [3840,-2048]
+        ]],
+      }
+    },
+
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Statues Room",
+        "link": "https://wiki.supermetroid.run/Statues_Room",
+        "area": "Crateria",
+      },
+      "geometry": {
+        "type": "Polygon",
+        "coordinates": [[
+          [3840,-2048],
+          [3840,-2560],
+          [4096,-2560],
+          [4096,-2048]
+        ]],
+      }
+    },
+
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Crateria Super Room",
+        "link": "https://wiki.supermetroid.run/Crateria_Super_Room",
+        "area": "Crateria",
+      },
+      "geometry": {
+        "type": "Polygon",
+        "coordinates": [[
+          [4608,-2304],
+          [4608,-2560],
+          [5632,-2560],
+          [5632,-4096],
+          [4864,-4096],
+          [4864,-4352],
+          [5888,-4352],
+          [5888,-2304]
+        ]],
+      }
+    },
+
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Climb",
+        "link": "https://wiki.supermetroid.run/Climb",
+        "area": "Crateria",
+      },
+      "geometry": {
+        "type": "Polygon",
+        "coordinates": [[
+          [4352,-2304],
+          [4352,-4352],
+          [4096,-4352],
+          [4096,-4608],
+          [4608,-4608],
+          [4608,-4352],
+          [4864,-4352],
+          [4864,-4096],
+          [4608,-4096],
+          [4608,-2304]
+        ]],
+      }
+    },
+
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "The Moat",
+        "link": "https://wiki.supermetroid.run/The_Moat",
+        "area": "Crateria",
+      },
+      "geometry": {
+        "type": "Polygon",
+        "coordinates": [[
+          [8704,-1536],
+          [9216,-1536],
+          [9216,-1024],
+          [8704,-1024]
+        ]],
+      }
+    },
+
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "West Ocean",
+        "link": "https://wiki.supermetroid.run/West_Ocean",
+        "area": "Crateria",
+      },
+      "geometry": {
+        "type": "Polygon",
+        "coordinates": [[
+          [9216,0],
+          [11264,0],
+          [11264,-512],
+          [10752,-512],
+          [10752,-1024],
+          [11264,-1024],
+          [11264,-1536],
+          [9216,-1536]
+        ]],
+      }
+    },
+
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Forgotten Highway Kago Room",
+        "link": "https://wiki.supermetroid.run/Forgotten_Highway_Kago_Room",
+        "area": "Crateria",
+      },
+      "geometry": {
+        "type": "Polygon",
+        "coordinates": [[
+          [15616,-1024],
+          [15616,-2048],
+          [15872,-2048],
+          [15872,-1024]
+        ]],
+      }
+    },
+
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Crab Maze",
+        "link": "https://wiki.supermetroid.run/Crab_Maze",
+        "area": "Crateria",
+      },
+      "geometry": {
+        "type": "Polygon",
+        "coordinates": [[
+          [14848,-2048],
+          [14848,-2560],
+          [15872,-2560],
+          [15872,-2048]
+        ]],
+      }
+    },
+
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Unnamed",
+        "link": "#",
+        "area": "Crateria",
+      },
+      "geometry": {
+        "type": "Polygon",
+        "coordinates": [[
+          [14592,-2304],
+          [14592,-2560],
+          [14848,-2560],
+          [14848,-2304]
+        ]],
+      }
+    },
+
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Forgotten Highway Elevator",
+        "link": "https://wiki.supermetroid.run/Forgotten_Highway_Elevator",
+        "area": "Crateria",
+      },
+      "geometry": {
+        "type": "Polygon",
+        "coordinates": [[
+          [14592,-2560],
+          [14592,-2816],
+          [14848,-2816],
+          [14848,-2560]
+        ]],
+      }
+    },
+
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Pit Room",
+        "link": "https://wiki.supermetroid.run/Pit_Room",
+        "area": "Crateria",
+      },
+      "geometry": {
+        "type": "Polygon",
+        "coordinates": [[
+          [4608,-4352],
+          [4608,-4864],
+          [4864,-4864],
+          [4864,-4608],
+          [5376,-4608],
+          [5376,-4352]
+        ]],
+      }
+    },
+
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Unnamed",
+        "link": "#",
+        "area": "Crateria",
+      },
+      "geometry": {
+        "type": "Polygon",
+        "coordinates": [[
+          [5376,-4352],
+          [5376,-4608],
+          [5632,-4608],
+          [5632,-4352]
+        ]],
+      }
+    },
+
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Wrecked Ship Entrance",
+        "link": "https://wiki.supermetroid.run/Wrecked_Ship_Entrance",
+        "area": "Wrecked Ship",
+      },
+      "geometry": {
+        "type": "Polygon",
+        "coordinates": [[
+          [11264,-1024],
+          [11264,-1280],
+          [12288,-1280],
+          [12288,-1024]
+        ]],
+      }
+    },
+
   ]
-  
+
 };
